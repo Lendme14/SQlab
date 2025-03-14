743ea915926f29af410ce9f02b93d653f383d6ca
Nwabueze Christian Izuchukwu <izuchukwujnwabueze@gmail.com>
2025-03-13 19:29:52 -0700

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -9,3 +9,15 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
     console.log(`Backend server running at http://localhost:${port}`);
 });
+const express = require('express');
+const bodyParser = require('body-parser');
+const signupRoutes = require('./signup');
+
+const app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(signupRoutes);
+
+app.listen(3000, () => {
+    console.log('Server is running on port 3000');
+});
